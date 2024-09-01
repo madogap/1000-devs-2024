8de83c637dd645ad73a308f2e207ee509492c140
Marcus Augusto <madogap@gmail.com>
2024-08-31 22:50:51 -0300

    Primeira semana e segunda semana de aulas incluido no repositorio

--- aulas-1000-devs-2024/src/com/mado/mesttra/primeirasemana/HelloWorld.java
+++ aulas-1000-devs-2024/src/com/mado/mesttra/primeirasemana/HelloWorld.java
@@ -1,7 +1,18 @@
-public class HelloWorld{
+public class HelloWorld {
 
     public static void main(String[] args) {
+        System.err.println("====================================");
+        System.err.println("====================================");
+        System.err.println("====================================");
+        System.err.println("====================================");
+        System.err.println("====================================");
+        System.err.println("====================================");
         System.out.println("Hello World!");
+        System.err.println("fim do programa================================");
+        System.err.println("fim do programa================================");
+        System.err.println("fim do programa================================");
+        System.err.println("fim do programa================================");
+        System.err.println("fim do programa================================");
     }
 
 }
