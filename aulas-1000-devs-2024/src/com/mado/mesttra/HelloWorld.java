c40d2092954c573356070034c5751f8dfef76c9a
Marcus Augusto <madogap@gmail.com>
2024-09-16 16:26:37 -0300

    Adicionado e será ajustado o sout

--- aulas-1000-devs-2024/src/com/mado/mesttra/HelloWorld.java
+++ aulas-1000-devs-2024/src/com/mado/mesttra/HelloWorld.java
@@ -12,7 +12,24 @@ public class HelloWorld {
         System.err.println("fim do programa================================");
         System.err.println("fim do programa================================");
         System.err.println("fim do programa================================");
-        System.err.println("fim do programa================================");
+        System.err.println("fim do programa===Banco de dados\r\n" + //
+                "Computação em nuvem\r\n" + //
+                "Desenvolvimento de software\r\n" + //
+                "HTML\r\n" + //
+                "Programação (computação)\r\n" + //
+                "Estruturas de dados\r\n" + //
+                "\r\n" + //
+                "Adicionar\r\n" + //
+                "Graduate Entry\r\n" + //
+                "\r\n" + //
+                "Adicionar\r\n" + //
+                "JSON\r\n" + //
+                "\r\n" + //
+                "Adicionar\r\n" + //
+                "Microsserviços\r\n" + //
+                "\r\n" + //
+                "Adicionar\r\n" + //
+                "Windows Azure=============================");
     }
 
 }
